41b6c7f9a845d87e151b2c929438039cfefc0461
Matteo <matteo.gobbo12@gmail.com>
2021-02-22 23:33:21 +0100

    Valid palindrome with at most one removal

---
+++ palindrome-removal/palindrome-removal.js
@@ -0,0 +1,29 @@
+const string1 = "abcba";
+const string2 = "fabbof";
+const string3 = "abccab";
+
+function palindrome(str) {
+    let len = str.length;
+    let changes = 0;
+    if(len === 0 || len === 1)
+        return true;
+
+    if(str[0] === str[len - 1]) {
+        return palindrome(str.slice(1, len - 1));
+    } 
+
+    if(str[0] !== str[len-1] && len === 2) {
+        return true;
+    }
+
+    if(str[0] !== str[len-1] && len !== 2 && changes === 0) {
+        changes = changes + 1;
+        return palindrome(str.replace(str[0], str[len-1]).slice(0, len));
+    }
+
+    return false;
+}
+
+// console.log(palindrome(string1));
+console.log(palindrome(string2));
+// console.log(palindrome(string3));
